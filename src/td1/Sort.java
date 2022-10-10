3b48c3413bda7c1b929bf05188eb1c8bf179a68f
Thomas Prévost <thomasprevost85@gmail.com>
2022-10-10 11:07:38 +0200

    merge BubbleSort, InsSort -> Sort

--- src/td1/Sort.java
+++ src/td1/Sort.java
@@ -3,20 +3,16 @@ package td1;
 import java.util.Arrays;
 import java.util.Random;
 
-public class BubbleSort {
+public class Sort {
     private static final Random rnd = new Random();
     public static void main(String[] args) {
         int[] tab = new int[30];
         for (int i = 0; i < tab.length; i++) {
             tab[i] = rnd.nextInt(50);
         }
-        System.out.println("Tableau avant tri :");
-        for (int j : tab) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
-        System.out.println("Tableau après tri : " + Arrays.toString(bubbleSort(tab)));
-
+        System.out.println("Tableau avant tri : " + Arrays.toString(tab));
+        System.out.println("Tableau après tri bulle : " + Arrays.toString(bubbleSort(tab)));
+        System.out.println("Tableau après tri par insertion : " + Arrays.toString(insSort(tab)));
     }
 
     public static int[] bubbleSort(int[] table){
@@ -33,4 +29,20 @@ public class BubbleSort {
         }
         return table;
     }
+
+    public static int[] insSort(int[] table){
+        /* Trie une table par ordre croissant par tri par insertion */
+        int n = table.length;
+        for (int i = 1; i < n; ++i) {
+            int key = table[i];
+            int j = i - 1;
+            while (j >= 0 && table[j] > key) {
+                table[j + 1] = table[j];
+                j = j - 1;
+            }
+            table[j + 1] = key;
+        }
+        return table;
+    }
+
 }
