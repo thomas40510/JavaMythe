0d898c2f5e374a0cf6947aaf95901245aa3b7097
Thomas Prévost <thomasprevost85@gmail.com>
2022-11-11 20:24:00 +0100

    moved Log to utilities

--- src/td3/LibraryMgr.java
+++ src/td3/LibraryMgr.java
@@ -1,5 +1,6 @@
 package td3;
 import td3.Documents.*;
+import utilities.Log;
 
 import java.util.ArrayList;
 
@@ -7,22 +8,6 @@ public class LibraryMgr {
     /*
     A simple Library Manager
      */
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-
-    public static class Log{
-        public void e(String message){
-            System.out.println(ANSI_RED + "[Log/E]: " + message + ANSI_RESET);
-        }
-        public void i(String message){
-            System.out.println("[Log/I]: " + message);
-        }
-        public void d(String message){
-            System.out.println(ANSI_BLUE + "[Log/D]: " + message + ANSI_RESET);
-        }
-    }
     static Log Log = new Log();
 
     public static class Library{
---
+++ src/utilities/Log.java
@@ -0,0 +1,19 @@
+package utilities;
+public class Log{
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    public void e(String message){
+        System.out.println(ANSI_RED + "[Log/E]: " + message + ANSI_RESET);
+    }
+    public void i(String message){
+        System.out.println("[Log/I]: " + message);
+    }
+    public void d(String message){
+        System.out.println(ANSI_BLUE + "[Log/D]: " + message + ANSI_RESET);
+    }
+
+}
+
